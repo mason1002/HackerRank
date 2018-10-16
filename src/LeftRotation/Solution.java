cbd0126fd6bf6d5126538f853d261da2a2616e67
Mason <ms.catmissyou@gmail.com>
2018-10-16 14:18:02 +1100

    Left rotation solution

---
+++ src/LeftRotation/Solution.java
@@ -0,0 +1,22 @@
+package LeftRotation;
+/*
+     Author:Mason
+     2018/10/16 14:13
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int d = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[(i + n - d) % n] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
